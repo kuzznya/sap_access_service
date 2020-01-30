d84d010a1125c06e1398717765b4090241515a0e
cherchok <viktor.yurzanov@gmail.com>
2020-01-30 10:00:13 +0300

    off test

--- src/main/java/com/alpe/sap_access_service/controller/APIController.java
+++ src/main/java/com/alpe/sap_access_service/controller/APIController.java
@@ -32,7 +32,7 @@ public class APIController {
 
 
     //TODO change to PostMapping
-    @GetMapping("/auth")
+    @PostMapping("/auth")
     ResponseEntity<?> authorize(@RequestParam(name = "system") String systemName,
                          @RequestParam String username,
                          @RequestParam String password,

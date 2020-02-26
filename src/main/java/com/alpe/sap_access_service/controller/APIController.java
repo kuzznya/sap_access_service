b8cefb71e3dd0873f42665589ab98bc4e96e3834
kuzznya <ikuz_2002@mail.ru>
2020-02-26 11:39:51 +0300

    Small refactoring

--- src/main/java/com/alpe/sap_access_service/controller/APIController.java
+++ src/main/java/com/alpe/sap_access_service/controller/APIController.java
@@ -2,7 +2,6 @@ package com.alpe.sap_access_service.controller;
 
 import com.alpe.sap_access_service.SapAccessServiceApplication;
 import com.alpe.sap_access_service.services.AvailableAppsService;
-import com.alpe.sap_access_service.services.TableService;
 import com.alpe.sap_access_service.services.sessions.Session;
 import com.alpe.sap_access_service.services.sessions.SessionsService;
 import com.sun.xml.messaging.saaj.SOAPExceptionImpl;
@@ -10,8 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import org.springframework.security.access.AccessDeniedException;
 
 import java.util.LinkedList;
 import java.util.Set;

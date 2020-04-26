03bbac359082796c2a25672c7da1a4ce73aa18fd
kuzznya <ikuz_2002@mail.ru>
2020-04-26 18:33:10 +0300

    feat: SSL requirement param in properties file & from env var

--- src/main/java/com/alpe/sap_access_service/security/SecurityConfig.java
+++ src/main/java/com/alpe/sap_access_service/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.alpe.sap_access_service.service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -17,20 +18,22 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired UsersService usersService;
+    private Environment env;
+
+    private UsersService usersService;
+
+    public SecurityConfig(@Autowired Environment env, @Autowired UsersService usersService) {
+        this.env = env;
+        this.usersService = usersService;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                // Disable CORS & CSRF protection
-                .cors()
-                .and()
+                // Disable CSRF protection
                 .csrf().disable()
                 .headers().frameOptions().disable()
                 // Require TLS
-                .and()
-                .requiresChannel()
-                .anyRequest().requiresSecure()
                 .and()
                 // Add custom authentication filter
                 .addFilterAfter(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
@@ -43,8 +46,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/**").authenticated()
                 .and()
                 // Stateless HTTP sessions (for REST architecture)
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        ;
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        // Require SSL if 'requests.require-secure' is 'true' in application.properties
+        String requireSecure;
+        try {
+            requireSecure = env.getProperty("requests.require-secure");
+        } catch (Exception ex) {
+            requireSecure = null;
+        }
+        if (requireSecure == null || requireSecure.toLowerCase().equals("true"))
+            http.requiresChannel().anyRequest().requiresSecure();
+
     }
 
     @Bean(name = "tokenAuthenticationFilter")

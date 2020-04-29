13790f84943b8f947c4b8df2b59b814760b74097
kuzznya <ikuz_2002@mail.ru>
2020-04-29 21:18:08 +0300

    refactor: Simplify code of AppsController

--- src/main/java/com/alpe/sap_access_service/apps/controller/AppsController.java
+++ src/main/java/com/alpe/sap_access_service/apps/controller/AppsController.java
@@ -1,5 +1,6 @@
 package com.alpe.sap_access_service.apps.controller;
 
+import com.alpe.sap_access_service.apps.model.SAPApplication;
 import com.alpe.sap_access_service.security.model.TokenAuthentication;
 import com.alpe.sap_access_service.security.model.User;
 import com.alpe.sap_access_service.apps.service.AvailableAppsService;
@@ -9,7 +10,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -22,17 +26,14 @@ public class AppsController {
     }
 
     @GetMapping("/apps")
-    ResponseEntity<?> getApplications(TokenAuthentication auth) {
+    @ResponseBody
+    List<SAPApplication> getApplications(TokenAuthentication auth) {
         User user;
+        user = (User) auth.getPrincipal();
         try {
-            user = (User) auth.getPrincipal();
-        } catch (Exception ex) {
-            return new ResponseEntity<>("Not authorized", HttpStatus.UNAUTHORIZED);
-        }
-        try {
-            return new ResponseEntity<>(appsService.getAvailableApplications(user), HttpStatus.OK);
+            return appsService.getAvailableApplications(user);
         } catch (SOAPExceptionImpl ex) {
-            return new ResponseEntity<>("Not authorized", HttpStatus.UNAUTHORIZED);
+            throw new RuntimeException();
         }
     }
 }
